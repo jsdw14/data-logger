aa2c84bef0f821c0c11612d45ca21edbc0119191
jsdw14 <jsdw@icloud.com>
2023-05-15 12:41:19 +1000

    A - single log, B - continuous log

--- main.ts
+++ main.ts
@@ -9,8 +9,34 @@ input.onButtonPressed(Button.A, function () {
     )
     basic.showIcon(IconNames.Giraffe)
 })
+input.onButtonPressed(Button.B, function () {
+    if (logging) {
+        logging = false
+    } else {
+        logging = true
+    }
+})
+let logging = false
 basic.showIcon(IconNames.Giraffe)
 datalogger.setColumnTitles(
 "sound",
 "light"
 )
+logging = false
+let icon = 0
+loops.everyInterval(500, function () {
+    if (logging) {
+        datalogger.log(
+        datalogger.createCV("sound", input.soundLevel()),
+        datalogger.createCV("light", input.lightLevel())
+        )
+        icon += 1
+        if (icon % 2 == 0) {
+            basic.showIcon(IconNames.SmallDiamond)
+        } else {
+            basic.showIcon(IconNames.Diamond)
+        }
+    } else {
+        basic.showIcon(IconNames.Giraffe)
+    }
+})
