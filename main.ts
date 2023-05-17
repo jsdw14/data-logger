3f4914c0ed6476197d6fd96b97aa485e5abe3d98
jsdw14 <jsdw@icloud.com>
2023-05-17 20:17:16 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,10 +13,9 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(log_num)
     datalogger.log(
     datalogger.createCV("sound", input.soundLevel()),
-    datalogger.createCV("temperature", weatherbit.temperature()),
-    datalogger.createCV("soil moisture", -1)
+    datalogger.createCV("temperature", weatherbit.temperature() / 100),
+    datalogger.createCV("soil moisture", weatherbit.soilMoisture())
     )
-    basic.showIcon(IconNames.Yes)
 })
 let log_num = 0
 let logging = false
@@ -29,15 +28,17 @@ datalogger.setColumnTitles(
 )
 logging = false
 log_num = 0
+let continuous_log = 0
+datalogger.deleteLog()
 loops.everyInterval(500, function () {
     if (logging) {
-        log_num += 1
+        continuous_log += 1
         datalogger.log(
         datalogger.createCV("sound", input.soundLevel()),
-        datalogger.createCV("temperature", weatherbit.temperature()),
-        datalogger.createCV("soil moisture", weatherbit.soilMoisture())
+        datalogger.createCV("temperature", weatherbit.temperature() / 100),
+        datalogger.createCV("soil moisture", -1)
         )
-        if (log_num % 2 == 0) {
+        if (continuous_log % 2 == 0) {
             basic.showIcon(IconNames.SmallDiamond)
         } else {
             basic.showIcon(IconNames.Diamond)
